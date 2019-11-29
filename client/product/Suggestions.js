eefccc040ee04262a1692aae5741c5a054ae6131
AliChaudhary <alichaudhary100@gmail.com>
2019-11-29 17:04:34 +0500

    Update Suggestions.js

--- client/product/Suggestions.js
+++ client/product/Suggestions.js
@@ -9,8 +9,10 @@ import {Link} from 'react-router-dom'
 import ViewIcon from 'material-ui-icons/Visibility'
 import Icon from 'material-ui/Icon'
 import Divider from 'material-ui/Divider'
-import Card, { CardContent, CardMedia } from 'material-ui/Card'
+import Card, {CardContent, CardMedia} from 'material-ui/Card'
 import AddToCart from './../cart/AddToCart'
+import { Grid } from 'material-ui'
+
 
 const styles = theme => ({
   root: theme.mixins.gutters({
@@ -79,48 +81,50 @@ class Suggestions extends Component {
   render() {
     const {classes} = this.props
     return (<div>
-      <Paper className={classes.root} elevation={4}>
-        <Typography type="title" className={classes.title}>
-          {this.props.title}
-        </Typography>
-        {this.props.products.map((item, i) => {
-            return <span key={i}>
-              <Card className={classes.card}>
-                <CardMedia
-                  className={classes.cover}
-                  image={'/api/product/image/'+item._id}
-                  title={item.name}
-                />
-                <div className={classes.details}>
-                  <CardContent className={classes.content}>
-                    <Link to={'/product/'+item._id}><Typography type="title" component="h3" className={classes.productTitle} color="primary">{item.name}</Typography></Link>
-                    <Link to={'/shops/'+item.shop._id}>
-                      <Typography type="subheading" className={classes.subheading}>
-                        <Icon className={classes.icon}>shopping_basket</Icon> {item.shop.name}
-                      </Typography>
-                    </Link>
-                    <Typography component="p" className={classes.date}>
+      
+          <Paper className={classes.root} elevation={4}>
+            <Typography type="title" className={classes.title}>
+              {this.props.title}
+            </Typography>
+            {this.props.products.map((item, i) => {
+              return <span key={i}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cover}
+                    image={'/api/product/image/'+item._id}
+                    title={item.name}
+                  />
+                  <div className={classes.details}>
+                    <CardContent className={classes.content}>
+                      <Link to={'/product/'+item._id}><Typography type="title" component="h3" className={classes.productTitle} color="primary">{item.name}</Typography></Link>
+                      <Link to={'/shops/'+item.shop._id}>
+                        <Typography type="subheading" className={classes.subheading}>
+                          <Icon className={classes.icon}>shopping_basket</Icon> {item.shop.name}
+                        </Typography>
+                      </Link>
+                      <Typography component="p" className={classes.date}>
                         Added on {(new Date(item.created)).toDateString()}
-                    </Typography>
-                  </CardContent>
-                  <div className={classes.controls}>
-                    <Typography type="subheading" component="h3" className={classes.price} color="primary">Rs {item.price}</Typography>
+                      </Typography>
+                    </CardContent>
+                    <div className={classes.controls}>
+                      <Typography type="subheading" component="h3" className={classes.price} color="primary">Rs {item.price}</Typography>
                       <span className={classes.actions}>
                         <Link to={'/product/'+item._id}>
                           <IconButton color="secondary" dense="dense">
-                            <ViewIcon className={classes.iconButton}/>
+                            <ViewIcon className={classes.iconButton} />
                           </IconButton>
                         </Link>
-                        <AddToCart item={item}/>
+                        <AddToCart item={item} />
                       </span>
                     </div>
                   </div>
                 </Card>
-                <Divider/>
+                <Divider />
               </span>
             })
-          }
-      </Paper>
+            }
+          </Paper>
+
     </div>)
   }
 }
