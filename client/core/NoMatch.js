ed3b077c0c814baf311d3fd64fa9b9cff2283ad7
AliChaudhary <alichaudhary100@gmail.com>
2019-11-29 17:05:06 +0500

    Create NoMatch.js

---
+++ client/core/NoMatch.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export const NoMatch = () =>{
+    return(
+        <div>
+            <h3>Page Not found</h3>
+            <a href="/">Return to Home</a>
+        </div>
+        
+    )
+}
